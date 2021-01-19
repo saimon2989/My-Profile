6d8f57ef965fd45319df1e3969376a836f318074
Simón Orozco Henao <saimon2989@gmail.com>
2021-01-19 18:23:59 -0500

    CDesign Changes, add Bootstrap styles to DOM

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 
 // Nav-bar sticky
 
-$(document).ready(function(){
-	var altura = $('.nav-main').offset().top;	$(window).on('scroll', function(){
-		if ( $(window).scrollTop() > altura ){
-			$('.nav-main').addClass('navbar-fixed');
-		} else {
-			$('.nav-main').removeClass('navbar-fixed');
-		}
-	}
-	);
-});
+// $(document).ready(function(){
+// 	var altura = $('.nav-main').offset().top;	$(window).on('scroll', function(){
+// 		if ( $(window).scrollTop() > altura ){
+// 			$('.nav-main').addClass('navbar-fixed');
+// 		} else {
+// 			$('.nav-main').removeClass('navbar-fixed');
+// 		}
+// 	}
+// 	);
+// });
 
